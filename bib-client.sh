6cce19e3f1ff5b054d049d22a7e878cdac4f3b11
Heitor Freitas Ferreira <heitor.ff@ufu.br>
2024-06-11 20:48:37 -0300

    removendo uma pasta q tava pq foi sem querer e criação dos arquivos necessários para submissão do trabalho

---
+++ bib-client.sh
@@ -0,0 +1,1 @@
+echo "Não implementado ainda" # TODO: Ao terminar a implementação do cliente do portal, trocar para o comando correto
---
+++ bib-server.sh
@@ -0,0 +1,2 @@
+
+echo "Não implementado ainda" # TODO: Ao terminar a implementação do server do portal, trocar para o comando correto
