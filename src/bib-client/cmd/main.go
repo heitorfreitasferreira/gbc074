1a8c81d0b18d6147c8d49e86ac2eb0e38c7a7bb7
Ricmaloy <ricardozamboni021@gmail.com>
2024-09-05 19:52:03 -0300

    feat(bib-client): create handlers on main

--- src/bib-client/cmd/main.go
+++ src/bib-client/cmd/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"library-manager/bib-client/internal/handlers"
+	"log"
+	"os"
+
+	api "library-manager/shared/api/bib"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func main() {
-	log.Printf("Hello, World! CMS Client")
+	host := flag.String("host", "localhost", "Host to connect to")
+	port := flag.String("port", "50052", "Port to listen on")
+
+	flag.Parse()
+
+	conn, err := grpc.NewClient(fmt.Sprintf("%s:%s", *host, *port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Não foi possível conectar: %v", err)
+		panic(err)
+	}
+
+	defer conn.Close()
+	client := api_cad.NewPortalCadastroClient(conn)
+	for {
+		handler := handlers.Choose(*host, *port)
+
+		if err := handler(client); err != nil {
+			fmt.Printf("\nErro ao executar handler: %v", err)
+			fmt.Println("Pressione ENTER...")
+
+			bufio.NewReader(os.Stdin).ReadBytes('\n')
+		}
+	}
 }
