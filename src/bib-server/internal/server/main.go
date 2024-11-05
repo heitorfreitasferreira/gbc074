70eb71e9aa27d2d7833937a2cdc7ebb077daad90
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-11-05 12:21:13 -0300

    fix: make project compile for further corrections

--- src/bib-server/internal/server/main.go
+++ src/bib-server/internal/server/main.go
@@ -1,290 +1,305 @@
 package server
 
 import (
-    "bytes"
-    "context"
-    "encoding/json"
-    "errors"
-    "fmt"
-    "io"
-    "log"
-    "net/http"
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 
-    "library-manager/bib-server/internal/database"
-    "library-manager/bib-server/internal/queue/handlers"
-    api_bib "library-manager/shared/api/bib"
-    api_cad "library-manager/shared/api/cad"
-    "library-manager/shared/utils"
+	book_database "library-manager/book-database/database"
+	api_bib "library-manager/shared/api/bib"
+	"library-manager/user-database/database"
 )
 
+// TODO: Usar endpoints validos para obter os dados necessários.
+// TODO: Se basear em como o arquivo era antes (https://github.com/heitorfreitasferreira/gbc074/blob/897c656f9661339bc2329405fcfc1fbb886c3c0b/src/bib-server/internal/server/main.go) para fazer as requisições ao raft.
+// Implementar regra de negócio que tiver faltando nos endpoints raft (user-database e book-database).
+
 type Server struct {
-    api_cad.UnimplementedPortalCadastroServer
-    userDatabaseAddr string // Endereço http do banco, ex. http://localhost:21000
-    bookDatabaseAddr string
-    raftServerAddr   string // Endereço http do servidor Raft, ex. http://localhost:22000
+	api_bib.UnsafePortalBibliotecaServer
+	userDatabaseAddr string // Endereço http do banco, ex. http://localhost:21000
+	bookDatabaseAddr string
 }
 
-func NewServer(userDatabaseAddr, bookDatabaseAddr, raftServerAddr string) *Server {
-    return &Server{
-        userDatabaseAddr: userDatabaseAddr,
-        bookDatabaseAddr: bookDatabaseAddr,
-        raftServerAddr:   raftServerAddr,
-    }
+func NewServer(userDatabaseAddr, bookDatabaseAddr string) *Server {
+	return &Server{
+		userDatabaseAddr: userDatabaseAddr,
+		bookDatabaseAddr: bookDatabaseAddr,
+	}
 }
 
 var qos byte = 2
 
 func (s *Server) RealizaEmprestimo(stream api_bib.PortalBiblioteca_RealizaEmprestimoServer) error {
-    for {
-        data, err := stream.Recv()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            return err
-        }
+	for {
+		data, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
 
-        jsonData, err := json.Marshal(data)
-        if err != nil {
-            log.Printf("Erro ao converter dados do empréstimo para JSON: %v", err)
-            return err
-        }
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			log.Printf("Erro ao converter dados do empréstimo para JSON: %v", err)
+			return err
+		}
 
-        resp, err := http.Post(fmt.Sprintf("%s/realiza-emprestimo", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-        if err != nil {
-            log.Printf("Erro ao realizar empréstimo: %v", err)
-            return err
-        }
-        defer resp.Body.Close()
+		resp, err := http.Post(fmt.Sprintf("%s/realiza-emprestimo", s.bookDatabaseAddr), "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			log.Printf("Erro ao realizar empréstimo: %v", err)
+			return err
+		}
+		defer resp.Body.Close()
 
-        if resp.StatusCode != http.StatusOK {
-            log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-            return errors.New(resp.Status)
-        }
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+			return errors.New(resp.Status)
+		}
 
-        var status api_bib.Status
-        if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
-            log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-            return err
-        }
+		var status api_bib.Status
+		if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
+			log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+			return err
+		}
+	}
 
-        if err := stream.Send(&status); err != nil {
-            return err
-        }
-    }
-
-    return nil
+	return nil
 }
 
 func (s *Server) RealizaDevolucao(stream api_bib.PortalBiblioteca_RealizaDevolucaoServer) error {
-    for {
-        data, err := stream.Recv()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            return err
-        }
+	for {
+		data, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
 
-        jsonData, err := json.Marshal(data)
-        if err != nil {
-            log.Printf("Erro ao converter dados da devolução para JSON: %v", err)
-            return err
-        }
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			log.Printf("Erro ao converter dados da devolução para JSON: %v", err)
+			return err
+		}
 
-        resp, err := http.Post(fmt.Sprintf("%s/realiza-devolucao", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-        if err != nil {
-            log.Printf("Erro ao realizar devolução: %v", err)
-            return err
-        }
-        defer resp.Body.Close()
+		resp, err := http.Post(fmt.Sprintf("%s/realiza-devolucao", s.bookDatabaseAddr), "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			log.Printf("Erro ao realizar devolução: %v", err)
+			return err
+		}
+		defer resp.Body.Close()
 
-        if resp.StatusCode != http.StatusOK {
-            log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-            return errors.New(resp.Status)
-        }
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+			return errors.New(resp.Status)
+		}
 
-        var status api_bib.Status
-        if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
-            log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-            return err
-        }
+		var status api_bib.Status
+		if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
+			log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+			return err
+		}
+	}
 
-        if err := stream.Send(&status); err != nil {
-            return err
-        }
-    }
-
-    return nil
+	return nil
 }
 
-func (s *Server) BloqueiaUsuarios(ctx context.Context, request *api_cad.ListaIdentificadores) (*api_cad.Status, error) {
-    jsonData, err := json.Marshal(request)
+func (s *Server) BloqueiaUsuarios(ctx context.Context, request *api_bib.Vazia) (*api_bib.Status, error) {
+	jsonData, err := json.Marshal(request)
 
-    if err != nil {
-        log.Printf("Erro ao converter dados dos identificadores para JSON: %v", err)
-        return &api_cad.Status{Status: 1, Msg: "Erro ao converter dados para JSON"}, nil
-    }
+	if err != nil {
+		log.Printf("Erro ao converter dados dos identificadores para JSON: %v", err)
+		return &api_bib.Status{Status: 1, Msg: "Erro ao converter dados para JSON"}, nil
+	}
 
-    resp, err := http.Post(fmt.Sprintf("%s/bloqueia-usuarios", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        log.Printf("Erro ao bloquear usuários: %v", err)
-        return &api_cad.Status{Status: 1, Msg: "Erro ao bloquear usuários"}, nil
-    }
-    defer resp.Body.Close()
+	resp, err := http.Post(fmt.Sprintf("%s/bloqueia-usuarios", s.userDatabaseAddr), "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("Erro ao bloquear usuários: %v", err)
+		return &api_bib.Status{Status: 1, Msg: "Erro ao bloquear usuários"}, nil
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-        return &api_cad.Status{Status: 1, Msg: "Erro na resposta do servidor Raft"}, errors.New(resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+		return &api_bib.Status{Status: 1, Msg: "Erro na resposta do servidor Raft"}, errors.New(resp.Status)
+	}
 
-    var status api_cad.Status
-    if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
-        log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-        return &api_cad.Status{Status: 1, Msg: "Erro ao decodificar resposta do servidor Raft"}, err
-    }
+	var status api_bib.Status
+	if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
+		log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+		return &api_bib.Status{Status: 1, Msg: "Erro ao decodificar resposta do servidor Raft"}, err
+	}
 
-    return &status, nil
+	return &status, nil
 }
 
-func (s *Server) LiberaUsuarios(ctx context.Context, request *api_cad.ListaIdentificadores) (*api_cad.Status, error) {
-    jsonData, err := json.Marshal(request)
-    if err != nil {
-        log.Printf("Erro ao converter dados dos identificadores para JSON: %v", err)
-        return &api_cad.Status{Status: 1, Msg: "Erro ao converter dados para JSON"}, nil
-    }
+func (s *Server) LiberaUsuarios(ctx context.Context, request *api_bib.Vazia) (*api_bib.Status, error) {
+	jsonData, err := json.Marshal(request)
+	if err != nil {
+		log.Printf("Erro ao converter dados dos identificadores para JSON: %v", err)
+		return &api_bib.Status{Status: 1, Msg: "Erro ao converter dados para JSON"}, nil
+	}
 
-    resp, err := http.Post(fmt.Sprintf("%s/libera-usuarios", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        log.Printf("Erro ao liberar usuários: %v", err)
-        return &api_cad.Status{Status: 1, Msg: "Erro ao liberar usuários"}, nil
-    }
+	resp, err := http.Post(fmt.Sprintf("%s/libera-usuarios", s.userDatabaseAddr), "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("Erro ao liberar usuários: %v", err)
+		return &api_bib.Status{Status: 1, Msg: "Erro ao liberar usuários"}, nil
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-        return &api_cad.Status{Status: 1, Msg: "Erro na resposta do servidor Raft"}, errors.New(resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+		return &api_bib.Status{Status: 1, Msg: "Erro na resposta do servidor Raft"}, errors.New(resp.Status)
+	}
 
-    var status api_cad.Status
+	var status api_bib.Status
 
-    if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
-        log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-        return &api_cad.Status{Status: 1, Msg: "Erro ao decodificar resposta do servidor Raft"}, err
-    }
+	if err := json.NewDecoder(resp.Body).Decode(&status); err != nil {
+		log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+		return &api_bib.Status{Status: 1, Msg: "Erro ao decodificar resposta do servidor Raft"}, err
+	}
 
-    return &status, nil
+	return &status, nil
 }
 
-func (s *Server) ListaUsuariosBloqueados(ctx context.Context, request *api_cad.Empty) (*api_cad.ListaUsuarios, error) {
-    jsonData, err := json.Marshal(request)
-    if err != nil {
-        log.Printf("Erro ao converter dados para JSON: %v", err)
-        return nil, err
-    }
+func (s *Server) ListaUsuariosBloqueados(req *api_bib.Vazia, stream api_bib.PortalBiblioteca_ListaUsuariosBloqueadosServer) error {
+	resp, err := http.Get(fmt.Sprintf("%s/lista-usuarios-bloqueados", s.userDatabaseAddr))
+	if err != nil {
+		log.Printf("Erro ao listar usuários bloqueados: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
 
-    resp, err := http.Post(fmt.Sprintf("%s/lista-usuarios-bloqueados", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        log.Printf("Erro ao listar usuários bloqueados: %v", err)
-        return nil, err
-    }
-    defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+		return errors.New(resp.Status)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-        return nil, errors.New(resp.Status)
-    }
+	var listaUsuarios []database.User
+	if err := json.NewDecoder(resp.Body).Decode(&listaUsuarios); err != nil {
+		log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+		return err
+	}
 
-    var listaUsuarios api_cad.ListaUsuarios
-    if err := json.NewDecoder(resp.Body).Decode(&listaUsuarios); err != nil {
-        log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-        return nil, err
-    }
+	// TODO: Resgatar livros que fizeram usuário ficar bloqueado.
+	for _, usuario := range listaUsuarios {
+		protoUser := database.UserToProto(usuario)
 
-    return &listaUsuarios, nil
+		blockedUser := &api_bib.UsuarioBloqueado{
+			Usuario: &api_bib.Usuario{
+				Cpf:  protoUser.Cpf,
+				Nome: protoUser.Nome,
+			},
+			Livros: make([]*api_bib.Livro, 0),
+		}
+
+		stream.Send(blockedUser)
+	}
+
+	return nil
 }
 
-func (s *Server) ListaLivrosEmprestados(ctx context.Context, request *api_bib.Empty) (*api_bib.ListaLivros, error) {
-    jsonData, err := json.Marshal(request)
-    if err != nil {
-        log.Printf("Erro ao converter dados para JSON: %v", err)
-        return nil, err
-    }
+func (s *Server) ListaLivrosEmprestados(req *api_bib.Vazia, stream api_bib.PortalBiblioteca_ListaLivrosEmprestadosServer) error {
+	resp, err := http.Get(fmt.Sprintf("%s/lista-livros-emprestados", s.bookDatabaseAddr))
+	if err != nil {
+		log.Printf("Erro ao listar livros emprestados: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
 
-    resp, err := http.Post(fmt.Sprintf("%s/lista-livros-emprestados", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        log.Printf("Erro ao listar livros emprestados: %v", err)
-        return nil, err
-    }
-    defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+		return errors.New(resp.Status)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-        return nil, errors.New(resp.Status)
-    }
+	// TODO: corrigir listagem, o endpoint do raft não retorna nesse formato.
+	var listaLivros []book_database.Book
+	if err := json.NewDecoder(resp.Body).Decode(&listaLivros); err != nil {
+		log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+		return err
+	}
 
-    var listaLivros api_bib.ListaLivros
-    if err := json.NewDecoder(resp.Body).Decode(&listaLivros); err != nil {
-        log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-        return nil, err
-    }
+	for _, livro := range listaLivros {
+		stream.Send(&api_bib.Livro{
+			Isbn:   string(livro.Isbn),
+			Titulo: livro.Titulo,
+			Autor:  livro.Autor,
+			Total:  livro.Total,
+		})
+	}
 
-    return &listaLivros, nil
+	return nil
 }
 
-func (s *Server) ListaLivrosEmFalta(ctx context.Context, request *api_bib.Empty) (*api_bib.ListaLivros, error) {
-    jsonData, err := json.Marshal(request)
-    if err != nil {
-        log.Printf("Erro ao converter dados para JSON: %v", err)
-        return nil, err
-    }
+func (s *Server) ListaLivrosEmFalta(req *api_bib.Vazia, stream api_bib.PortalBiblioteca_ListaLivrosEmFaltaServer) error {
+	resp, err := http.Get(fmt.Sprintf("%s/lista-livros-em-falta", s.bookDatabaseAddr))
+	if err != nil {
+		log.Printf("Erro ao listar livros em falta: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
 
-    resp, err := http.Post(fmt.Sprintf("%s/lista-livros-em-falta", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        log.Printf("Erro ao listar livros em falta: %v", err)
-        return nil, err
-    }
-    defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+		return errors.New(resp.Status)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-        return nil, errors.New(resp.Status)
-    }
+	// TODO: corrigir listagem, o endpoint do raft não retorna nesse formato.
+	var listaLivros []book_database.Book
+	if err := json.NewDecoder(resp.Body).Decode(&listaLivros); err != nil {
+		log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+		return err
+	}
 
-    var listaLivros api_bib.ListaLivros
-    if err := json.NewDecoder(resp.Body).Decode(&listaLivros); err != nil {
-        log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-        return nil, err
-    }
+	for _, livro := range listaLivros {
+		stream.Send(&api_bib.Livro{
+			Isbn:   string(livro.Isbn),
+			Titulo: livro.Titulo,
+			Autor:  livro.Autor,
+			Total:  livro.Total,
+		})
+	}
 
-    return &listaLivros, nil
+	return nil
 }
 
-func (s *Server) PesquisaLivro(ctx context.Context, request *api_bib.PesquisaLivroRequest) (*api_bib.ListaLivros, error) {
-    jsonData, err := json.Marshal(request)
-    if err != nil {
-        log.Printf("Erro ao converter dados da pesquisa para JSON: %v", err)
-        return nil, err
-    }
+func (s *Server) PesquisaLivro(req *api_bib.Criterio, stream api_bib.PortalBiblioteca_PesquisaLivroServer) error {
 
-    resp, err := http.Post(fmt.Sprintf("%s/pesquisa-livro", s.raftServerAddr), "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        log.Printf("Erro ao pesquisar livro: %v", err)
-        return nil, err
-    }
-    defer resp.Body.Close()
+	// TODO: parse do critério de busca. Enviar critério de busca como parâmetro da requisição.
+	// Tratar endpoint do raft para usar critério de busca.
+	resp, err := http.Get(fmt.Sprintf("%s/pesquisa-livro", s.bookDatabaseAddr))
+	if err != nil {
+		log.Printf("Erro ao pesquisar livro: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
-        return nil, errors.New(resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Erro na resposta do servidor Raft: %v", resp.Status)
+		return errors.New(resp.Status)
+	}
 
-    var listaLivros api_bib.ListaLivros
-    if err := json.NewDecoder(resp.Body).Decode(&listaLivros); err != nil {
-        log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
-        return nil, err
-    }
+	var listaLivros []book_database.Book
+	if err := json.NewDecoder(resp.Body).Decode(&listaLivros); err != nil {
+		log.Printf("Erro ao decodificar resposta do servidor Raft: %v", err)
+		return err
+	}
 
-    return &listaLivros, nil
+	for _, livro := range listaLivros {
+		stream.Send(&api_bib.Livro{
+			Isbn:   string(livro.Isbn),
+			Titulo: livro.Titulo,
+			Autor:  livro.Autor,
+			Total:  livro.Total,
+		})
+	}
+
+	return nil
 }
